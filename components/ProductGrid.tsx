1b1731df12230d834b03565b2a086c2bb66a32ff
StepanKirichenko <kirichenkostepan@yandex.ru>
2023-02-24 13:06:16 +0300

    Add pagination component to product grid.

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -11,5 +11,26 @@ export default function ProductGrid({ products }: Props) {
     <ProductCard key={product.name} product={product} />
   ));
 
-  return <div className={styles.product_grid}>{productElements}</div>;
+  return (
+    <>
+      <div className={styles.product_grid}>{productElements}</div>
+      <Pagination />
+    </>
+  );
+}
+
+function Pagination() {
+  const pageLinkElements = [1, 2, 3, 4].map((page) => (
+    <a
+      href="#"
+      className={`${styles.pagination__link} ${
+        page === 1
+          ? styles.pagination__link_active
+          : styles.pagination__link_inactive
+      }`}
+    >
+      {page}
+    </a>
+  ));
+  return <div className={styles.pagination}>{pageLinkElements}</div>;
 }
