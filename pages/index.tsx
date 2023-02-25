c52f028e8c28affe9aead269b5b79a8d402b10b1
StepanKirichenko <kirichenkostepan@yandex.ru>
2023-02-25 15:30:09 +0300

    Add placeholder categories links to home page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,6 +33,63 @@ export default function Home() {
             />
           </div>
         </section>
+
+        <section className={styles.description_section}>
+          <div className="container row gap-small">
+            <div className="col align-start">
+              <p className={styles.description_paragraph}>
+                Наш интернет-магазин меда готов предложить самые разные сорта
+                этого вкусного лакомства, которое можно купить по весьма
+                демократичной цене. Наряду с ним мы реализуем и другие товары –
+                прополис, цветочную пыльцу, маточное молочко, пергу.
+              </p>
+              <p className={styles.description_paragraph}>
+                Все они обладают целебными свойствами и находят широкое
+                применение в медицине. Их с успехом используют для лечения
+                гипертонии, простудных и неврологических заболеваний, решения
+                проблем с опорно-двигательным аппаратом.
+              </p>
+            </div>
+            <div className="col align-end">
+              <p className={styles.description_paragraph_big}>
+                Мы радуем наших клиентов с 2002 года
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.product_categories_section}>
+          <div className={styles.categories_image_container}>
+            <Image
+              className={styles.categories_image}
+              src="/images/honey-bowl.png"
+              alt="мёд"
+              width={959}
+              height={822}
+            />
+          </div>
+          <div className={styles.categories_links}>
+            <div className={styles.category_link_container}>
+              <Link href="#" className={styles.category_link}>
+                Мёд <p className={styles.category_counter}>50</p>
+              </Link>
+              <hr className={styles.category_underline} />
+            </div>
+            <div className={styles.category_link_container}>
+              <Link href="#" className={styles.category_link}>
+                Чайные напитки <p className={styles.category_counter}>19</p>
+              </Link>
+              <hr className={styles.category_underline} />
+            </div>
+            <div className={styles.category_link_container}>
+              <Link href="#" className={styles.category_link}>
+                Варенье и конфитюры{" "}
+                <p className={styles.category_counter}>30</p>
+              </Link>
+              <hr className={styles.category_underline} />
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
