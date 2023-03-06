7430e0434149dd2b46aa58e241c07d150f8e6281
StepanKirichenko <kirichenkostepan@yandex.ru>
2023-03-06 16:49:46 +0300

    Finish ServiceCard and Services page.

--- components/ServiceCard/ServiceCard.tsx
+++ components/ServiceCard/ServiceCard.tsx
@@ -2,24 +2,28 @@ import Image from "next/image";
 import { SmallButtonLink } from "../Button";
 import styles from "./ServiceCard.module.css";
 
-export default function ServiceCard() {
+interface Props {
+  title: string;
+  img: string;
+  link: string;
+}
+
+export default function ServiceCard({ title, img, link }: Props) {
   return (
     <div className="col">
       <div className={styles.image_container}>
         <Image
           className={styles.image}
-          src="/images/services/1.png"
-          alt="банка с мёдом на столе"
+          src={img}
+          alt={title}
           width={450}
           height={602}
         />
       </div>
       <hr className={styles.divider} />
-      <h3 className={styles.title}>
-        Расфасовка мёда под вашей торговой маркой
-      </h3>
+      <h3 className={styles.title}>{title}</h3>
       <div className={styles.button + " " + "row"}>
-        <SmallButtonLink href="#">Подробнее</SmallButtonLink>
+        <SmallButtonLink href={link}>Подробнее</SmallButtonLink>
       </div>
     </div>
   );
--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -12,12 +12,36 @@ export default function Services() {
         <div className="container">
           <h1 className={styles.page_heading}>Услуги</h1>
           <div className={styles.grid}>
-            <ServiceCard />
-            <ServiceCard />
-            <ServiceCard />
-            <ServiceCard />
-            <ServiceCard />
-            <ServiceCard />
+            <ServiceCard
+              title="Расфасовка мёда под вашей торговой маркой"
+              img="/images/services/1.png"
+              link="#"
+            />
+            <ServiceCard
+              title="собственная торговая марка"
+              img="/images/services/2.png"
+              link="#"
+            />
+            <ServiceCard
+              title="разработка и выпуск медовых композиций"
+              img="/images/services/3.png"
+              link="#"
+            />
+            <ServiceCard
+              title="разработка индивидуального заказа"
+              img="/images/services/4.png"
+              link="#"
+            />
+            <ServiceCard
+              title="этикетирование"
+              img="/images/services/5.png"
+              link="#"
+            />
+            <ServiceCard
+              title="переработка мёда в крем"
+              img="/images/services/6.png"
+              link="#"
+            />
           </div>
         </div>
       </main>
