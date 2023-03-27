793c30bd7c2987e63f30094f4c60529f31860020
StepanKirichenko <kirichenkostepan@yandex.ru>
2023-03-27 13:31:14 +0300

    Add loading state to ProductGrid.

--- components/ProductGrid/ProductGrid.tsx
+++ components/ProductGrid/ProductGrid.tsx
@@ -5,19 +5,32 @@ import { Product } from "@/utils/products";
 interface Props {
   products: Product[];
   columns?: number;
+  isLoading?: boolean;
 }
 
-export default function ProductGrid({ products, columns = 4 }: Props) {
+export default function ProductGrid({
+  products,
+  columns = 4,
+  isLoading = false,
+}: Props) {
   const productElements = products.map((product) => (
     <ProductCard key={product.id} product={product} />
   ));
 
   return (
-    <div
-      style={{ gridTemplateColumns: `repeat(${columns}, 1fr)` }}
-      className={styles.product_grid}
-    >
-      {productElements}
-    </div>
+    <>
+      {isLoading ? (
+        <div className={styles.loading}>
+          <img src="/images/icons/loading.svg" className={styles.loadingIcon} />
+        </div>
+      ) : (
+        <div
+          style={{ gridTemplateColumns: `repeat(${columns}, 1fr)` }}
+          className={styles.product_grid}
+        >
+          {productElements}
+        </div>
+      )}
+    </>
   );
 }
