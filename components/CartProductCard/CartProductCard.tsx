a4914719067260ade742b8e1ea993e31e7ba3eea
StepanKirichenko <kirichenkostepan@yandex.ru>
2023-03-30 16:51:59 +0300

    Display correct info in cart product card.

--- components/CartProductCard/CartProductCard.tsx
+++ components/CartProductCard/CartProductCard.tsx
@@ -1,5 +1,5 @@
 import { CartContext } from "@/contexts/CartContext";
-import { Product } from "@/utils/products";
+import { Product, Weight } from "@/utils/products";
 import Image from "next/image";
 import { useContext } from "react";
 import RatingDisplay from "../RatingDisplay";
@@ -40,7 +40,7 @@ export default function CartProductCard({ product, amount }: Props) {
               что пчелы собирают пыльцу и нектар не с какого-либо определённого
               растения, а с разнотравья.
             </p>
-            <p className={styles.product_year}>2022</p>
+            <p className={styles.product_year}>{product.year}</p>
           </div>
         </div>
         <div className={styles.button_container}>
@@ -65,9 +65,9 @@ export default function CartProductCard({ product, amount }: Props) {
         </div>
         <div className={styles.button_container}>
           <button className={styles.empty_button}>
-            <h4 className={styles.button_title}>Объём</h4>
+            <h4 className={styles.button_title}>Вес</h4>
             <div className={styles.button_content}>
-              <p>0,5</p>
+              <p>{weightToDisplayString(product.weight)}</p>
             </div>
           </button>
         </div>
@@ -76,4 +76,14 @@ export default function CartProductCard({ product, amount }: Props) {
       <hr className={styles.divider} />
     </div>
   );
+}
+
+function weightToDisplayString(value: Weight): string {
+  if (value === "350") {
+    return "0,35";
+  } else if (value === "500") {
+    return "0,5";
+  } else {
+    return "1";
+  }
 }
